e415c2095654e8b386d8395bd1daf6c3e6d5296e
odyssic <stevenbuddenjr@gmail.com>
2020-02-04 22:59:38 -0500

    flex box working now some text not

--- myscript.js
+++ myscript.js
@@ -1,21 +1,7 @@
-// function capitalizeFirstLetter(string) {
-//     return string[0].toUpperCase() + string.slice(1);
-// }
-
-// const wholeName = customers.map(function(customer) {
-//     return (
-//         capitalizeFirstLetter(customer.name.first) +
-//         " " +
-//         capitalizeFirstLetter(customer.name.last)
-//     );
-
-//     console.log("cap name ran");
-// });
-
 for (let customer of customers) {
     container_block = document.querySelector("#container");
 
-    container_block.classList.add("container");
+    container_block.classList.add("flex-box");
 
     const picture = document.createElement("img");
     const pictureDiv = container_block.parentElement.appendChild(picture);
@@ -23,38 +9,47 @@ for (let customer of customers) {
 
     picture.src = customer.picture.large;
 
-    const name = document.createElement("div");
-    container_block.parentElement.appendChild(name);
-
-    const email = document.createElement("div");
-    container_block.parentElement.appendChild(email);
-
-    const addressOne = document.createElement("div");
-    container_block.parentElement.appendChild(addressOne);
-
-    const addressTwo = document.createElement("div");
-    container_block.parentElement.appendChild(addressTwo);
-
-    const dateOfBirth = document.createElement("div");
-    container_block.parentElement.appendChild(dateOfBirth);
-
-    customerSince = document.createElement("div");
-    container_block.parentElement.appendChild(customerSince);
-
-    name.innerHTML = customer.name.first.last;
-    email.innerHTML = customer.email;
-    addressOne.innerHTML = customer.location.street;
-    addressTwo.innerHTML = customer.location.city;
-    dateOfBirth.innerHTML = customer.dob;
-    customerSince.innerHTML = customer.registered;
-
-    // var img = document.createElement("img");
-
-    // img.src = "image.png";
-    // var src = document.getElementById("x");
-
-    // src.appendChild(img);
+    function capitalizeFirstLetter(string) {
+        return string[0].toUpperCase() + string.slice(1);
+    }
+    capitalizeFirstLetter(customer.name.first) +
+        " " +
+        capitalizeFirstLetter(customer.name.last);
 }
+
+const name = document.createElement("div");
+const nameDiv = container_block.parentElement.appendChild(name);
+nameDiv.classList.add("name");
+
+const email = document.createElement("div");
+emailDiv = container_block.parentElement.appendChild(email);
+emailDiv.classList.add("email");
+
+const addressOne = document.createElement("div");
+container_block.parentElement.appendChild(addressOne);
+
+const addressTwo = document.createElement("div");
+container_block.parentElement.appendChild(addressTwo);
+
+const dateOfBirth = document.createElement("div");
+container_block.parentElement.appendChild(dateOfBirth);
+
+customerSince = document.createElement("div");
+container_block.parentElement.appendChild(customerSince);
+
+name.innerHTML = wholeName;
+email.innerHTML = customer.email;
+addressOne.innerHTML = customer.location.street;
+addressTwo.innerHTML = customer.location.city;
+dateOfBirth.innerHTML = customer.dob;
+customerSince.innerHTML = customer.registered;
+
+// var img = document.createElement("img");
+
+// img.src = "image.png";
+// var src = document.getElementById("x");
+
+// src.appendChild(img);
 
 // let email = customer.email;
 
